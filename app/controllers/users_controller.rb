eef303615f0e5fe54360bc0daf15a681df31a075
Rafia Khandaker <rafiakhandaker@gmail.com>
2022-10-17 14:05:25 -0400

    commit tree 0058c6f0e48db983e5221723980a2e8405ce9118

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user, include: :items
+        else
+            render json: {error: "no user found"}, status: :not_found
+        end
+
+    end
+
+
 end
